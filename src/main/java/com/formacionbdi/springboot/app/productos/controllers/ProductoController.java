4408191a6e81c03eea9e165205c1082b6292e6bc
Roberto Rugama <roberto.rugama01@gmail.com>
2022-02-21 23:06:34 -0600

    Balanceo_carga_haciendo_uso_de_ribbon_a_travez_de_Feign
    Se agregó el parametro Puerto a la clase Producto para identificar la disponibilidad del servicio.
    
    2- la clase producto se  modificaron los metodos findAll y FindbyId para inyectar atravez de injeccion de dependiecias y a si poder obtener que puerto que respondio por mayor disponibilidad para validar el balanceo de carga funciona correctamente

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,8 +1,11 @@
 package com.formacionbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +15,29 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 
 @RestController    //Convierte_la_respuesta_a_una_estructura_jSON
 public class ProductoController {
+	
+	@Autowired
+	private Environment  env;
+	
+	@Value("${server.port}")
+	private Integer port;
 	@Autowired  // Injeccion_de_dependencia
 	private IProductoService productoService;
 	
-	@GetMapping("/listar") //Se_mapea el_metodo a_una_ruta_de_un_endpoint_para_que_se_comunique los otros servicios 
+	@GetMapping("/listar") //Se_mapea el_metodo a_una_ruta_de_un_endpoint_para_que_se_comunique_los_otros_servicios 
 	public List<Producto> listar(){
-		return productoService.findAll();
+		return productoService.findAll().stream().map(producto->{
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
+			return producto;
+		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/show/{id}")
 	public Producto detalle(@PathVariable Long id) {
-		return productoService.findById(id);
+		Producto producto = productoService.findById(id);
+		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
+		return producto;
 	}
 }
